d51d98724bd3eaec0b9ad1ae64348332eace015d
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2021-06-28 19:15:34 +0530

    Heroku update

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -20,11 +20,8 @@ class PageWrapper extends Component {
         e.preventDefault();
         localStorage.clear();
         this.props.changeState(null);
+        signout();
         location.replace("/login")
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-        console.log('User signed out.');
-        });
     }
     render() {
         return (
